58dcfeea53f161c9dd6e1323bb1122e222e65ad2
Pigeonlover <bravefoot@outlook.com>
2025-08-30 17:58:43 +0100

    Updates: - style.css = added buttons' hover and active states. Removed pigeon image's hover scale effect as I thought it wasn't needed. - script.js = added the barley shop item functionality.

--- script.js
+++ script.js
@@ -26,9 +26,13 @@ document.getElementById("barley-sps-amount").textContent = barleySPS;
 
 setInterval(function () {
   totalSPS = barleySPS + cornSPS + sunflowerSPS;
-  document.getElementById("total-sps-amount").textContent = totalSPS;
+  document.getElementById("total-sps-amount").textContent =
+    Math.round(totalSPS * 10) / 10;
   seedsAmount += totalSPS;
-  document.getElementById("seed-amount").textContent = seedsAmount;
+  document.getElementById("seed-amount").textContent = Math.floor(seedsAmount);
+
+  totalClicks =
+    1 + barleyIncreaseClicks + cornIncreaseClicks + sunflowerIncreaseClicks;
 }, 1000);
 
 //when the user clicks on the pigeon, seedsAmount decrease by 1. The poop amount then goes up by 1 after 2 seconds.
@@ -36,22 +40,43 @@ setInterval(function () {
 document.getElementById("pigeon-image").addEventListener("click", function () {
   if (seedsAmount > 0) {
     seedsAmount -= totalClicks;
-    document.getElementById("seed-amount").textContent = seedsAmount;
+    document.getElementById("seed-amount").textContent =
+      Math.floor(seedsAmount);
     setTimeout(function () {
       poopAmount += totalClicks;
-      document.getElementById("poop-amount").textContent = poopAmount;
+      document.getElementById("poop-amount").textContent =
+        Math.floor(poopAmount);
     }, 2000);
   } else {
     alert("You don't have enough seeds!");
   }
 });
 
-// Barley shop item button: when clicked, will decrease poop amount by cost. Cost then increases by 10% rounded up. Increases SPS by 1.
+// Barley shop item button: when clicked, will decrease poop amount by cost. Cost then increases by 10%. Increases barleySPS by 0.1. Increase totalClicks by 0.2 every purchase.
 
-//you will need functions to contain the game logic
-//to create the logic for the shop upgrades:
-//- OPTION 1: you could have a function per upgrade
-//- OPTION 2: you could have a reusable function that works for ALL the upgrades
+document.getElementById("barley-button").addEventListener("click", function () {
+  let barleyCost = document.getElementById("barley-button-cost").textContent;
+  if (poopAmount >= barleyCost) {
+    poopAmount -= barleyCost;
+    document.getElementById("poop-amount").textContent = Math.floor(poopAmount);
+
+    barleyCost = barleyCost * 1.1;
+    document.getElementById("barley-button-cost").textContent =
+      Math.floor(barleyCost);
+
+    barleySPS += 0.1;
+    document.getElementById("barley-sps-amount").textContent =
+      Math.round(barleySPS * 10) / 10;
+
+    barleyIncreaseClicks += 0.2;
+    document.getElementById("barley-clicks-amount").textContent =
+      Math.round(barleyIncreaseClicks * 10) / 10;
+  } else {
+    alert("You don't have enough poop!");
+  }
+});
+
+//=============================================================
 
 //Tip on local storage:
 // - Make sure the local storage values are updated after the user buys an upgrade or when the user clicks on the cookie
@@ -83,11 +108,3 @@ document.getElementById("pigeon-image").addEventListener("click", function () {
 //after you complete this task, you should see the upgrades on your page
 
 //================================================================
-
-//the interval
-
-setInterval(function () {
-  cookieCount += cps; //cookieCount = cookieCount + cps
-  //update the DOM to reflect the changes in the values
-  //save the values in local storage
-}, 1000);
